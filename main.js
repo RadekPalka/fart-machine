df4b08d851eb7700f9930e09b2e9ad96a8db2fd5
Radoslaw Palka <radekpalka@gmail..com>
2023-02-08 18:20:03 +0100

    create main.js and make it works

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const button = document.querySelector("button")
 
-const makeSomeNoise = () =>{
+const makeSomeNoise = async () =>{
+  button.disabled = true
+  button.textContent = "Proszę czekać"
+  const number = Math.floor(Math.random()*9 +1)
+  const fart = new Audio(`./fart noises/fart${number}.mp3`)
+  await fart.play()
+  const duration = fart.duration* 1000
+  setTimeout(()=>{
+    button.disabled = false
+  button.textContent = "Naciśnij przycisk"
+  },duration)  
   
 }
 
