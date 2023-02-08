1bee7a8ca5ff9fc8c6eb50dcdcbe50a1ac559413
Radoslaw Palka <radekpalka@gmail..com>
2023-02-08 19:58:44 +0100

    add disabled class to style.css and main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const wait = time =>{
   setTimeout(()=>{
   button.disabled = false
   button.textContent = "Naciśnij przycisk"
+  button.classList.remove("disabled")
   },time)  
 
 }
@@ -11,6 +12,7 @@ const wait = time =>{
 const makeSomeNoise = async () =>{
   button.disabled = true
   button.textContent = "Proszę czekać"
+  button.classList.add("disabled")
   const number = Math.floor(Math.random()*9 +1)
   const fart = new Audio(`./fart noises/fart${number}.mp3`)
   await fart.play()
